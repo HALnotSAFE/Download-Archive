a3d89cb45a572058dc29608a115fe8f63ebbdb30
Adam Major <126974886+HALnotSAFE@users.noreply.github.com>
2023-10-23 02:15:45 -0400

    Add files via upload

---
+++ ArchiveDeepStorage.ps1
@@ -0,0 +1,16 @@
+﻿# Set variables for source and destination folders
+$sourceFolder1 = "C:\DLArchive"
+$sourceFolder2 = "C:\DLManifest"
+$destinationFolder = "S:\IT\Adam Major\Archive"
+
+# Move files from first source folder
+Get-ChildItem $sourceFolder1 | Copy-Item -Destination $destinationFolder
+
+# Remove files from first source folder
+Get-ChildItem $sourceFolder1 | Remove-Item -Force
+
+# Move files from second source folder
+Get-ChildItem $sourceFolder2 | Copy-Item -Destination $destinationFolder
+
+# Remove files from second source folder
+Get-ChildItem $sourceFolder2 | Remove-Item -Force
